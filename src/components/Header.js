7d60d16477b7fe273a83c71c7a00a3b318f6c09e
Abhi <abhiramims333@gmail.com>
2023-12-30 19:03:28 -0500

    Add style to nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import Logo from '../assets/logo.jpg';
+import '../styles/Header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Header = () => {
+function Header() {
   return (
-    <header>
-      <h1>Portfolio</h1>
-      <nav>
-        <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container className='navigationbar'>
+        <img className='logo' src={Logo} alt='Logo'/>
+        <Navbar.Brand href="#home">Abhi Murali </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
 export default Header;
+
