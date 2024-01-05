086ea2bef4ff7103ab2aa41d6b5d02f4eb069277
Abhi <abhiramims333@gmail.com>
2024-01-04 22:44:45 -0500

    Change images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import IMG1 from '../assets/img1.jpg'
-import IMG2 from '../assets/img2.jpg'
-import IMG3 from '../assets/img3.jpg'
+import IMG1 from '../assets/img1.jpg';
+import IMG2 from '../assets/img2.jpg';
+import IMG3 from '../assets/img3.jpg';
+import IMG4 from '../assets/img4.jpg';
+import IMG5 from '../assets/img5.jpg';
+import IMG6 from '../assets/img6.jpg';
+import IMG7 from '../assets/img7.jpg';
+import IMG8 from '../assets/img8.jpg';
+import IMG9 from '../assets/img9.jpg';
 import '../styles/Home.css'
 
 const Home = () => {
@@ -10,13 +16,31 @@ const Home = () => {
       <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade " data-bs-ride="carousel" data-bs-interval="5000">
   <div className="carousel-inner slides">
     <div className="carousel-item active">
-      <img src={IMG1} className="d-block w-100" alt="1 of slideshow"/>
+      <img src={IMG1} className="d-block img-fluid" alt="1 of slideshow"/>
     </div>
     <div className="carousel-item">
-      <img src={IMG2} className="d-block w-100" alt="2 of slideshow"/>
+      <img src={IMG2} className="d-block img-fluid" alt="2 of slideshow"/>
     </div>
     <div className="carousel-item">
-      <img src={IMG3} className="d-block w-100" alt="3 of slideshow"/>
+      <img src={IMG3} className="d-block img-fluid" alt="3 of slideshow"/>
+    </div>
+    <div className="carousel-item">
+      <img src={IMG4} className="d-block img-fluid" alt="4 of slideshow"/>
+    </div>
+    <div className="carousel-item">
+      <img src={IMG5} className="d-block img-fluid" alt="5 of slideshow"/>
+    </div>
+    <div className="carousel-item">
+      <img src={IMG6} className="d-block img-fluid" alt="6 of slideshow"/>
+    </div>
+    <div className="carousel-item">
+      <img src={IMG7} className="d-block img-fluid" alt="7 of slideshow"/>
+    </div>
+    <div className="carousel-item">
+      <img src={IMG8} className="d-block img-fluid" alt="8 of slideshow"/>
+    </div>
+    <div className="carousel-item">
+      <img src={IMG9} className="d-block w-100" alt="9 of slideshow"/>
     </div>
   </div>
   <button className="carousel-control-prev hidebuttons" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,9 @@ import IMG3 from '../assets/img3.jpg';
 import IMG4 from '../assets/img4.jpg';
 import IMG5 from '../assets/img5.jpg';
 import IMG6 from '../assets/img6.jpg';
+import IMG7 from '../assets/img7.jpg';
+import IMG8 from '../assets/img8.jpg';
+import IMG9 from '../assets/img9.jpg';
 
 
 
@@ -24,9 +27,9 @@ const Projects = () => {
         <img src={IMG4} alt="Gallery  4"/>
         <img src={IMG5} alt="Gallery  5"/>
         <img src={IMG6} alt="Gallery  6"/>
-        <img src={IMG1} alt="Gallery  1"/>
-        <img src={IMG2} alt="Gallery 2"/>
-        <img src={IMG3} alt="Gallery  3"/>
+        <img src={IMG7} alt="Gallery  7"/>
+        <img src={IMG8} alt="Gallery  8"/>
+        <img src={IMG9} alt="Gallery  9"/>
     </div>
       
 </div>
